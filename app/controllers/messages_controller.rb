b21dbdbdd354dba6a4b53553a7f1ef146fa892e6
masahiro7777777 <e6esusgk@softbank.ne.jp>
2020-03-09 21:22:12 +0900

    Automatic-updating

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       respond_to do |format|
         format.json
+        # create.json.jbuilderへ
       end
     else
       @messages = @group.messages.includes(:user)
