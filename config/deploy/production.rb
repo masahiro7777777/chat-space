4cf5b55a9cab0a6d86cbd1abd6585ae262e05dd2
masahiro7777777 <e6esusgk@softbank.ne.jp>
2020-03-11 16:27:20 +0900

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server 'config/deploy.rb54.178.127.177', user: 'ec2-user', roles: %w{app db web}
+server '54.178.127.177', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
